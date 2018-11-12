a0fda2627e07e20b35281e6bcbe7780821ee367e
Adam Chen Wei <adamchenwei@gmail.com>
2018-11-12 12:49:43 -0500

    feat(jest): working basic jest setup

---
+++ src/components/HelloComponent/HelloComponent.test.js
@@ -0,0 +1,8 @@
+import { shallowMount } from '@vue/test-utils'
+import HelloComponent from './HelloComponent.vue'
+
+describe('HelloComponent', () => {
+  it('should work', () => {
+    expect(1).toEqual(1);
+  })
+});
