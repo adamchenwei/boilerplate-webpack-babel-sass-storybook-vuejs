a7c7764a71ad60887d5414726c095187b405b4a4
Adam Chen Wei <adamchenwei@gmail.com>
2018-11-12 12:00:54 -0500

    init(): basic setup with sass webpack eslint babel and jest

---
+++ src/components/App/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="full-width center-content">
+    <hello-component name="World" />
+  </div>
+</template>
+<script>
+import HelloComponent from '../HelloComponent/HelloComponent.vue'
+export default {
+  components: {
+    HelloComponent
+  }
+}
+</script>
+
+<style lang="scss">
+  h2 {
+    color: mediumseagreen;
+    .wave {
+      color: orchid;
+    }
+  }
+</style>
