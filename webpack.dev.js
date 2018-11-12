06266e32ff105cd5d22dedd5b373163eafb0c61e
Adam Chen Wei <adamchenwei@gmail.com>
2018-11-12 18:43:27 -0500

    chore(): continue trying, add dist, and tweak config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,26 @@
-// Hum... dont really need dev, since I really just need storybook!
-// const merge = require('webpack-merge');
-// const common = require('./webpack.common.js');
+Hum... dont really need dev, since I really just need storybook!
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-// module.exports = merge(common, {
-//   mode: 'development',
-//   devtool: 'inline-source-map',
-//   devServer: {
-//     contentBase: './dist'
-//   }
-// })
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    }),
+    new CopyWebpackPlugin([{
+      // NOTE: it does not really do anything, unless we have a asset folder, that needed no compression
+      from: './static/',
+      to: './static/',
+      toType: 'dir'
+    }]),
+  ],
+})
