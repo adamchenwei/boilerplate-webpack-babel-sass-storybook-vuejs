4cbf0356c358aae9937ebd50bd2d4f051b444116
Adam Chen Wei <adamchenwei@gmail.com>
2018-11-12 15:01:44 -0500

    broken(story): when a story added for vue, its broken

---
+++ src/components/HelloComponent/HelloComponent.stories.js
@@ -0,0 +1,11 @@
+import { storiesOf } from '@storybook/vue'
+import HelloComponent from './HelloComponent.vue'
+
+storiesOf('HelloComponent', module)
+  // NOTE: it has problem render!
+  // .add('story as a template', () => '<HelloComponent name="Adam" />')
+  .add('default', () => ({
+    components: { HelloComponent },
+    template: '<HelloComponent name="Adam"/>'
+  }))
+  
