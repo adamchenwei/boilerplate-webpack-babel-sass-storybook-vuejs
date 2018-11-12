c9f689f569f7795029718bbb91196ef9fc46b9f6
Adam Chen Wei <adamchenwei@gmail.com>
2018-11-12 15:12:15 -0500

    feat(storybook): working sass config for storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,33 @@
+//basic extend
+const path = require("path");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"],
+        include: path.resolve(__dirname, "../")
+      }
+    ]
+  }
+};
+
+
+//full + default
+
+// const path = require("path");
+
+// module.exports = (baseConfig, env, defaultConfig) => {
+//   // Extend defaultConfig as you need.
+
+//   // For example, add typescript loader:
+//   defaultConfig.module.rules.push({
+//     test: /\.(ts|tsx)$/,
+//     include: path.resolve(__dirname, "../src"),
+//     loader: require.resolve("ts-loader")
+//   });
+//   defaultConfig.resolve.extensions.push(".ts", ".tsx");
+
+//   return defaultConfig;
+// };
