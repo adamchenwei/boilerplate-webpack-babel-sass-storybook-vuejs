349b0f4317988e25bd7648f63e84f23e9b6a35b4
Adam Chen Wei <adamchenwei@gmail.com>
2018-11-12 12:53:24 -0500

    test(sample): test works with vue test-util

--- src/components/HelloComponent/HelloComponent.test.js
+++ src/components/HelloComponent/HelloComponent.test.js
@@ -3,6 +3,9 @@ import HelloComponent from './HelloComponent.vue'
 
 describe('HelloComponent', () => {
   it('should work', () => {
-    expect(1).toEqual(1);
+    const wrapper = shallowMount(HelloComponent, {
+      propsData: { name: 'myname' }
+    });
+    expect(wrapper.findAll('.wave').length).toEqual(1)
   })
 });
