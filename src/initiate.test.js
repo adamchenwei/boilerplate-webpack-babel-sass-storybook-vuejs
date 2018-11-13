6e997940207570cd830ccd240dd80740f5f10b80
Adam Chen Wei <adamchenwei@gmail.com>
2018-11-13 17:11:31 -0500

    feat(storyshot): fully configed with per repo snapshots

---
+++ src/initiate.test.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import { shallowMount } from '@vue/test-utils'
+import App from './components/App/App.vue'
+import initStoryshots, { multiSnapshotWithOptions } from '@storybook/addon-storyshots'
+
+initStoryshots({
+  storyKindRegex: /^((?!.*?DontTest).)*$/,
+  test: multiSnapshotWithOptions({
+
+    // createNodeMock: (element) => {
+    //   if (element.type === TextareaThatUsesRefs) {
+    //     return document.createElement('textarea')
+    //   }
+    // },
+  })
+})
+
+describe('App', () => {
+  it('should work', () => {
+    const wrapper = shallowMount(App)
+    expect(wrapper.findAll('.full-width').length).toEqual(1)
+  })
+})
